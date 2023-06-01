91deb364f78acd00d5e0941e6538d7db5aa8c24e
martin marchessi <niitram.mm@gmail.com>
2023-06-01 01:06:08 -0300

    mayus

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ import Clients from "./views/clients/Clients";
 import Appointments from "./views/appointments/Appointments";
 import ServicesControl from "./views/Services Control/ServicesControl";
 import Professionals from "./views/Professionals/Professionals";
-import ContactForm from "./views/ContactForm/contactForm";
+import ContactForm from "./views/contactForm/contactForm";
 import FooterAll from "./components/footerAll/FooterAll";
 import NotFound from "./components/notFound/NotFound";
 import DetailService from "./views/detailService/detailService";
